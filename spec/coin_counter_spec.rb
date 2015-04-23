090f8443ed47db158dcc2175b529eefa02d7727b
jordan carlson & cory ribson <student@epicodus.com>
2015-04-23 10:21:20 -0700

    add method coin_counter

--- spec/coin_counter_spec.rb
+++ spec/coin_counter_spec.rb
@@ -3,6 +3,6 @@ require('coin_counter')
 
 describe('Fixnum#.coin_counter') do
   it("will take cent amount and return the type of change") do
-    expect(('79').coin_counter)to.(eq("three quarters four pennies"))
+    expect(('79').coin_counter)to.(eq("3 quarters, 0 dimes, 0 nickels, 4 pennies"))
   end
 end
