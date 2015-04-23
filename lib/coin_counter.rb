fba026b90c7aa13b8afc4339219a759ff26e63da
jordan carlson & cory ribson <student@epicodus.com>
2015-04-23 10:48:28 -0700

    Added coin_counter method code

--- lib/coin_counter.rb
+++ lib/coin_counter.rb
@@ -1,5 +1,22 @@
 class Fixnum
-  define_method(:coin_counter)
-
+  define_method(:coin_counter) do
+    change = self
+    quarters = 0
+    dimes = 0
+    nickels = 0
+    pennies = 0
+    output = ""
+    until change.==(0)
+      quarters = change./(25).floor
+      change = change % 25
+      dimes = change./(10).floor
+      change = change % 10
+      nickels = change./(5).floor
+      change = change % 5
+      pennies = change./(1)
+      change = change % 1
+      change
+    end
+    output = "" +quarters + " quarters, " + dimes + " dimes, " + nickels + "nickels, " + pennies + "pennies"
   end
 end
