ce2e262e9675fd5f0f044df7775d0f4e608ef00d
jordan carlson & cory ribson <student@epicodus.com>
2015-04-23 11:25:13 -0700

    add second spec and pass

--- lib/coin_counter.rb
+++ lib/coin_counter.rb
@@ -8,16 +8,32 @@ class Fixnum
     output = ""
     until change.==(0)
       quarters = change./(25).floor
+      if quarters.!=(0)
+        output = output.concat("#{quarters} quarters")
+      end
       change = change % 25
       dimes = change./(10).floor
+      if dimes.!=(0)
+        output = output.concat(", #{dimes} dimes")
+      end
       change = change % 10
       nickels = change./(5).floor
+      if nickels.!=(0)
+        output = output.concat(", #{nickels} nickels")
+      end
       change = change % 5
       pennies = change./(1)
+      if pennies.!=(0)
+        if pennies == (1)
+          output = output.concat(", #{pennies} penny")
+        else
+          output = output.concat(", #{pennies} pennies")
+        end
+      end
       change = change % 1
       change
     end
-    output = "#{quarters} quarters, #{dimes} dimes, #{nickels} nickels, #{pennies} pennies"
+    #output = "#{quarters} quarters, #{dimes} dimes, #{nickels} nickels, #{pennies} pennies"
     output
   end
 end
--- spec/coin_counter_spec.rb
+++ spec/coin_counter_spec.rb
@@ -3,6 +3,10 @@ require('coin_counter')
 
 describe('Fixnum#.coin_counter') do
   it("will take cent amount and return the type of change") do
-    expect((79).coin_counter).to(eq("3 quarters, 0 dimes, 0 nickels, 4 pennies"))
+    expect((79).coin_counter).to(eq("3 quarters, 4 pennies"))
+  end
+
+  it("will take 81 cents and return the change due") do
+    expect((81).coin_counter).to(eq("3 quarters, 1 nickels, 1 penny"))
   end
 end
